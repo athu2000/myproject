00a6f4b052112b11fc66b1bcb3a56e547973293a
athu2000 <athulmadhu2k@gmail.com>
2022-11-21 13:14:30 +0530

    commit

---
+++ bin/variable.dart
@@ -0,0 +1,15 @@
+void main(){
+  String name    = 'POCO M2 Pro';
+  String storage = '128GB';
+  int androidver = 12;
+  String conn    = '4G';
+  int mobile     = 9072875391;
+
+  print('******My Mobile Details******');
+  print('The name of my phone is $name');
+  print('It has a storage of $storage');
+  print('The Android version is $androidver');
+  print('It has the connectivity $conn');
+  print('Mobile Number : $mobile');
+
+}
